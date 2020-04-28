0e59cef69f488537e79e7a18f741692d0062e421
github username <github email address>
2020-04-28 02:24:12 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,6 +26,10 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+
+    if Preference.all.first.allow_create_songs == false
+      redirect_to songs_path
+    end
   end
 
   def create
